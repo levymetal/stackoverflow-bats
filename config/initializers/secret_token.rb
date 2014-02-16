49e165347ea0942d3eb3dc65e2ef0c8c258d8068
levymetal <noreply@levymetal.net>
2014-02-16 18:37:02 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bats::Application.config.secret_key_base = 'a624e23f8baa9852bc34672c227cb97afbbf3ea327fa88ba3aa5ba7eb3c62b098633926fd24b5de6a78a09e0ca9b9bae7f685fbed1150dfa8565dc43358e44d0'
